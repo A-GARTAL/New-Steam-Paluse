38b67b3fe83d8120d4f4c36d4681aa060d57e922
Hadi Mirzazade <140284812+A-GARTAL@users.noreply.github.com>
2025-01-12 23:05:20 +0330

    v0.4

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 const sideBarOptiopns = document.querySelectorAll('.side_option_label')
 const contentPages = document.querySelectorAll('.content_page')
-console.log(contentPages[0].id)
+const gameSearchBar = document.getElementById('gameSerachInput');
+const gameSearchIcon = document.getElementById('gameSerachIcon');
+let gameBanner = document.getElementById('game_banner')
+let gameName = document.getElementById('game_name')
+let gameDlcCount = document.getElementById('game_dlc_count')
+let gameReleaseDate = document.getElementById('game_release_date')
+let gamePlayerCount = document.getElementById('game_player_count')
+let gameDeveloper = document.getElementById('game_developer')
+let gamePublisher = document.getElementById('game_publisher')
+console.log(gameName)
+gameSearchBar.addEventListener('keyup',keyValidator)
+gameSearchIcon.addEventListener('click',getGameAppId)
+const url ="https://store.steampowered.com/api/appdetails?appids="
+// console.log(contentPages[0].id)
 
 function optionDetector(event)
 {
@@ -28,3 +41,55 @@ function setOptionListListener(list)
 }
 
 setOptionListListener(sideBarOptiopns);
+
+
+
+function keyValidator(event){
+  if (event.key == 'Enter' )
+    getGameAppId(event)
+}
+let gameAppId
+function getGameAppId(event){
+  console.log(gameSearchBar.value)
+  gameAppId = gameSearchBar.value;
+  // 1091500&cc=ar";
+  let finalURL = url+gameAppId+'&ar'
+  // console.log(finalURL)
+  gameInfoFetch(finalURL)
+}
+
+function gameInfoFetch(URL){
+
+const response = fetch(URL); 
+// یک تابع برای ارسال درخواست
+fetchData()
+async function fetchData() {
+    try {
+        // ارسال درخواست به سمت سرور
+        const response = await fetch(URL);
+        
+        // بررسی وضعیت پاسخ
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        // پارس کردن داده‌ها به صورت JSON
+        let data = await response.json();
+        
+        // ذخیره داده‌ها در یک متغی
+        // console.log(data[gameAppId].data.price_overview.final);
+        gameBanner.setAttribute('src',data[gameAppId].data.header_image);
+        gameName.innerHTML = data[gameAppId].data.name;
+        gameDlcCount.innerHTML = data[gameAppId].data.dlc.length;
+        gameReleaseDate.innerHTML = data[gameAppId].data.release_date.date;
+        // gamePlayerCount.innerHTML = data[gameAppId].data
+        gamePlayerCount.innerHTML = "Error!!!"
+        gameDeveloper.innerHTML = data[gameAppId].data.developers;
+        gamePublisher.innerHTML = data[gameAppId].data.publishers;
+
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+}
+
